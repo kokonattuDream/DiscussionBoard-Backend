67a3ae88118ca7adc3765921151b1e8cb11be293
kokonattuDream <kerjohn.chen@gmail.com>
2020-05-03 09:11:57 -0400

    init the project

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require("http");
+const app= express();
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    
+    res.end('Hello World\n');
+ });
+
+ server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+ })
