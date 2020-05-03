43586d99daa55c33a3f57032be69f1a3c5ac35e3
kokonattuDream <kerjohn.chen@gmail.com>
2020-05-03 09:43:13 -0400

    connected with mongodb

--- server.js
+++ server.js
@@ -2,15 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const http = require("http");
 const app= express();
+const cors = require('cors');
+const env = require("./env");
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const MongoClient = require('mongodb').MongoClient;
+const url = `mongodb+srv://${env.dev.db.user}:${env.dev.db.password}@discussion-board-cluster-e1mbo.mongodb.net/test?retryWrites=true&w=majority`;
+const client = new MongoClient(url, { useUnifiedTopology: true });
 
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    
-    res.end('Hello World\n');
+client.connect(function(err) {
+   if(err){
+      console.log(err);
+   }
+   console.log("Connected successfully to server");
+   //const collection = client.db(env.dev.db.dbName).collection(env.dev.db.collection);
+   client.close();
  });
 
- server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
- })
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.listen(3000, () => {
+   console.log("server running on port 3000");
+});
