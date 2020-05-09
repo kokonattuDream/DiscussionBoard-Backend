ce94a111ae193a117cef5fd4da42ea02b2bfebe0
kokonattuDream <kerjohn.chen@gmail.com>
2020-05-09 13:45:51 -0400

    add env

---
+++ env.js
@@ -0,0 +1,16 @@
+const dev = {
+    db: {
+        user:"adminUser",
+        password:"admin1234",
+        dbName:"discussion-board-db",
+        collection:"discussion-board-collection"
+    },
+    image_src:{
+        cloud_name: 'boardfiles',
+        api_key: '334986354856358',
+        api_secret: 'pao8wQZqgyyYsZBnEQyiAJXXANQ'
+    }
+}
+module.exports ={
+    dev
+}
