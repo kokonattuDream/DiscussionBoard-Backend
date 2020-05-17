1dec1df00fe53739fb2686faa077f95ec018a77a
kokonattuDream <kerjohn.chen@gmail.com>
2020-05-17 08:56:58 -0400

    update cache when add reply

--- controllers/replyController.js
+++ controllers/replyController.js
@@ -1,29 +1,44 @@
 const Reply = require("../models/reply");
 const User = require("../models/user");
 const Post = require("../models/post");
+const Cache = require("../lib/cache");
 
 exports.addReply = async (req, res) => {
     console.log(req.body);
     try {
       if(req.session.user){
+        /** 
         let user = await User.findOne({ username: req.body.user });
         console.log(user);
         if (!user) {
           res.status(404).send("username not found!");
-        }
+        }*/
 
         let post = await Post.findById(req.body.post);
         
         let newReply = Reply({
-          user: user,
+          user: req.session.user._id,
           text: req.body.reply,
           date: new Date()
         });
 
         await newReply.save();
         post.replies.push(newReply._id);
+        post.updated_date = newReply.date;
+
         await post.save();
-        console.log("post success");
+        
+        let replyCache = JSON.parse(JSON.stringify(newReply));
+        replyCache.user = {
+          username: req.session.user.username
+        }
+        let postCache = Cache.get(req.body.post);
+        if(postCache){
+          postCache.updated_date = post.updated_date;
+          postCache.replies.push(replyCache);
+          console.log(postCache);
+          Cache.set(req.body.post, postCache);
+        }
         
         res.status(200).json({ message: "Reply submitted" });
       } else {
