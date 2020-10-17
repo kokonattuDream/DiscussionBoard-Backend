61b34f1eae4805a8b07d37ca2c0e799200c25b2d
kokonattuDream <kerjohn.chen@gmail.com>
2020-10-17 17:39:16 -0400

    remove non-required methods

--- models/user.js
+++ models/user.js
@@ -1,17 +1,8 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt-nodejs');
 
 const userSchema = mongoose.Schema({
     username: {type: String, required: true, index: { unique: true }},
     password: {type: String, required: true}
 });
-
-userSchema.methods.encryptPassword = (password) => {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
-}
-
-userSchema.methods.checkPassword = (password) => {
-    return bcrypt.compareSync(password, this.password);
-}
 
 module.exports = mongoose.model('User', userSchema);
