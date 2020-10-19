b1c3ccd086b7827c741879f29bee17808eb048d1
kokonattuDream <kerjohn.chen@gmail.com>
2020-10-19 08:37:03 -0400

    remove not required header

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
    res.header("Access-Control-Allow-Origin", frontendUrl);
    res.header("Access-Control-Allow-Credentials", "true");
    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
    next();
 });
 app.use(cookieParser());
