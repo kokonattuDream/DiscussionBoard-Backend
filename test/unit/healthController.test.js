74e5c1be0e70280aedacdeb2e48d46c6a08b1673
kokonattuDream <kerjohn.chen@gmail.com>
2020-10-19 10:18:35 -0400

    changes for test

--- test/unit/healthController.test.js
+++ test/unit/healthController.test.js
@@ -1,10 +1,10 @@
-const mockingoose = require('mockingoose').default;;
 const httpMock = require('node-mocks-http');
 let controller = require('../../controllers/healthController');
 
 it("Test health check", async() =>{
     let req = httpMock.createRequest();
     let res = httpMock.createResponse();
+    res.statusCode = 500; //Original mock is 200, so we change to 500 for testing purpose
     await controller.healthCheck(req, res);
     expect(res.statusCode).toBe(200);
     expect(res._getJSONData()).toEqual({message: "Health Check Passed"});
